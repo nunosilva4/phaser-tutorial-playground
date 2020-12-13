fd8ecda925c90af02bc1991dbe9d51d8ec9d808e
nunosilva4 <nunosilvacmj@hotmail.com>
2020-12-13 16:40:29 +0000

    structure project

---
+++ libgdx/desktop/src/org/projetosdonuno/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.projetosdonuno.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.projetosdonuno.games.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
